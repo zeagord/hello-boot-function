cbc72464f53adc95637728b0ac4b8bed865e50f1
zeagord <rsganesan@palo-it.com>
2019-09-26 17:53:30 +0800

    adds grpc to boot

--- src/main/java/com/example/helloworld/HelloServiceImpl.java
+++ src/main/java/com/example/helloworld/HelloServiceImpl.java
@@ -2,14 +2,10 @@ package com.example.helloworld;
 
 import com.example.helloworld.HelloServiceGrpc.HelloServiceImplBase;
 import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
 
-public class HelloServiceImpl extends HelloServiceImplBase  {
-  @Override
-  public void hello(Hello.HelloRequest request, StreamObserver<Hello.HelloReply> responseObserver) {
-    responseObserver.onNext(buildReply(toMessage(request.getName())));
-    responseObserver.onCompleted();
-  }
-
+@Service
+public class HelloServiceImpl extends HelloServiceImplBase {
   static String toMessage(String name) {
     return "Hello, " + name + '!';
   }
@@ -18,4 +14,9 @@ public class HelloServiceImpl extends HelloServiceImplBase  {
     return Hello.HelloReply.newBuilder().setMessage(String.valueOf(message)).build();
   }
 
+  @Override
+  public void hello(Hello.HelloRequest request, StreamObserver<Hello.HelloReply> responseObserver) {
+    responseObserver.onNext(buildReply(toMessage(request.getName())));
+    responseObserver.onCompleted();
+  }
 }
--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,77 +1,42 @@
 package com.example.helloworld;
 
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
-import com.linecorp.armeria.server.Server;
-import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.docs.DocServiceBuilder;
-import com.linecorp.armeria.server.docs.DocServiceFilter;
 import com.linecorp.armeria.server.grpc.GrpcServiceBuilder;
+import com.linecorp.armeria.server.logging.AccessLogWriter;
+import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 import com.linecorp.armeria.spring.actuate.ArmeriaSpringActuatorAutoConfiguration;
 import io.grpc.protobuf.services.ProtoReflectionService;
-import io.grpc.reflection.v1alpha.ServerReflectionGrpc;
-import java.net.InetSocketAddress;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ImportAutoConfiguration(ArmeriaSpringActuatorAutoConfiguration.class)
 public class HelloWorldApplication {
 
-  private static Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
-
   public static void main(String[] args) throws Exception {
     SpringApplication.run(HelloWorldApplication.class, args);
-    final Server server = newServer(9000, 7443);
-
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      server.stop().join();
-      logger.info("Server has been stopped.");
-    }));
-
-    server.start().join();
-    final InetSocketAddress localAddress = server.activePort().get().localAddress();
-    final boolean isLocalAddress = localAddress.getAddress().isAnyLocalAddress() ||
-        localAddress.getAddress().isLoopbackAddress();
-    logger.info("Server has been started. Serving DocService at http://{}:{}/docs",
-        isLocalAddress ? "127.0.0.1" : localAddress.getHostString(), localAddress.getPort());
-  }
-  //
-  //@Bean
-  //public Supplier<Flux<String>> greet() {
-  //  return () -> Flux.just("Hello");
-  //}
-
-  static Server newServer(int httpPort, int httpsPort) throws Exception {
-    final Hello.HelloRequest exampleRequest = Hello.HelloRequest.newBuilder().setName("Armeria").build();
-    return new ServerBuilder()
-        .http(httpPort)
-        .https(httpsPort)
-        .tlsSelfSigned()
-        .service(new GrpcServiceBuilder()
-            .addService(new HelloServiceImpl())
-            // See https://github.com/grpc/grpc-java/blob/master/documentation/server-reflection-tutorial.md
-            .addService(ProtoReflectionService.newInstance())
-            .supportedSerializationFormats(GrpcSerializationFormats.values())
-            .enableUnframedRequests(true)
-            // You can set useBlockingTaskExecutor(true) in order to execute all gRPC
-            // methods in the blockingTaskExecutor thread pool.
-            // .useBlockingTaskExecutor(true)
-            .build())
-        // You can access the documentation service at http://127.0.0.1:8080/docs.
-        // See https://line.github.io/armeria/server-docservice.html for more information.
-        .serviceUnder("/docs", new DocServiceBuilder()
-            .exampleRequestForMethod(HelloServiceGrpc.SERVICE_NAME,
-                "Hello", exampleRequest)
-            .exampleRequestForMethod(HelloServiceGrpc.SERVICE_NAME,
-                "LazyHello", exampleRequest)
-            .exampleRequestForMethod(HelloServiceGrpc.SERVICE_NAME,
-                "BlockingHello", exampleRequest)
-            .exclude(DocServiceFilter.ofServiceName(ServerReflectionGrpc.SERVICE_NAME))
-            .build())
-        .build();
   }
 
+  @Bean
+  public ArmeriaServerConfigurator armeriaServerConfigurator(HelloServiceImpl helloService) {
+    return serverBuilder -> {
+      serverBuilder.service(new GrpcServiceBuilder()
+          .addService(helloService)
+          .addService(ProtoReflectionService.newInstance())
+          .supportedSerializationFormats(GrpcSerializationFormats.values())
+          .enableUnframedRequests(true)
+          .build()
+      );
+      serverBuilder.serviceUnder("/docs", new DocServiceBuilder()
+          .exampleRequestForMethod(HelloServiceGrpc.SERVICE_NAME, "Hello",
+              Hello.HelloRequest.newBuilder().setName("HelloWorld").build()).build()
+      );
+      serverBuilder.decorator(LoggingService.newDecorator());
+      serverBuilder.accessLogWriter(AccessLogWriter.combined(), true);
+    };
+  }
 }
